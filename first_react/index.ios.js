9e6fddeda80805f3e2c9bf4970da6e33b69a1141
johan parmar <parmar@johans-MacBook-Pro.local>
2015-05-30 18:47:59 +0200

    got button to work

--- first_react/index.ios.js
+++ first_react/index.ios.js
@@ -6,48 +6,55 @@
 
 var React = require('react-native');
 var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    TouchableHighlight,
+    Component,
+    AlertIOS
 } = React;
 
-var first_react = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Mjaueact Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-</View>
-    );
-  }
-});
+class first_react extends Component {
+    
+    render() {
+        return (
+                <View style={styles.container}>
+                <Text>
+                Welcome to React Native!
+                </Text>
+                <TouchableHighlight style={styles.button}
+                onPress={this.showAlert}>
+                <Text style={styles.buttonText}>Go</Text>
+                </TouchableHighlight>
+                </View>
+                );
+    }
+    
+    showAlert() {
+        AlertIOS.alert('Awesome Alert', 'This is my first React Native alert.', [{text: 'Thanks'}] );
+    }
+};
 
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+                               container: {
+                               flex: 1,
+                               justifyContent: 'center',
+                               alignItems: 'center',
+                               backgroundColor: '#FFFFFF'
+                               },
+                               buttonText: {
+                               fontSize: 18,
+                               color: 'white',
+                               alignSelf: 'center'
+                               },
+                               button: {
+                               height: 44,
+                               flexDirection: 'row',
+                               backgroundColor: '#48BBEC',
+                               alignSelf: 'stretch',
+                               justifyContent: 'center'
+                               }
+                               });
 
 AppRegistry.registerComponent('first_react', () => first_react);
